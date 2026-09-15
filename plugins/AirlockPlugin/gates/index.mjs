7131b910daf6964e9332990c9a57b23e81753421
Margarita T <MATAGABA@microsoft.com>
2026-09-15 05:28:55 -0700

    Add approvalGate to the gates export

--- plugins/AirlockPlugin/gates/index.mjs
+++ plugins/AirlockPlugin/gates/index.mjs
@@ -1,6 +1,6 @@
 import { createSecretsGate } from "./secrets/index.mjs";
 import { createNoOnlineWritesGate } from "./no-online-writes/index.mjs";
 import { createModelCatalogGate } from "./model-catalog/index.mjs";
-
+import { approvalGate } from "./approval/index.mjs";
 /** Trusted registrations only; never discover executable gates from the consumer repository. */
-export const gates = Object.freeze([createSecretsGate(), createNoOnlineWritesGate(), createModelCatalogGate()]);
+export const gates = Object.freeze([createSecretsGate(), createNoOnlineWritesGate(), createModelCatalogGate(), approvalGate()]);
