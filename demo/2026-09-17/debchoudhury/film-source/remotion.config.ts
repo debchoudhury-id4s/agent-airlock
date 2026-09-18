c445d7edd82bb1124fa7fa2c03cc3ba0739119f6
Debojit Choudhury <debchoudhury@microsoft.com>
2026-09-17 23:41:42 -0700

    add movie only. remove others

--- demo/2026-09-17/debchoudhury/film-source/remotion.config.ts
+++
@@ -1,9 +0,0 @@
-import {Config} from '@remotion/cli/config';
-
-Config.setVideoImageFormat('jpeg');
-Config.setCodec('h264');
-Config.setAudioCodec('aac');
-Config.setPixelFormat('yuv420p');
-Config.setCrf(18);
-Config.setConcurrency(4);
-Config.setBrowserExecutable('C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe');
